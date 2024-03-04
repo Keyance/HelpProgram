e39209809e9c06f75b0741ecba2ac14a0895f745
Keyance <klara.konopacova@seznam.cz>
2024-03-04 12:20:11 +0100

    jornal entry model commented

--- HelpLibrary/JournalEntryModel.cs
+++ HelpLibrary/JournalEntryModel.cs
@@ -8,9 +8,19 @@ namespace HelpLibrary
 {
     public class JournalEntryModel
     {
+        /// <summary>
+        /// The string with the writing for the journal entry. Inciated with " ".
+        /// </summary>
         string JournalWriting = " ";
+
+        /// <summary>
+        /// Enum varianble with feeling chosen for the diary entry.
+        /// </summary>
         PossibleFeeling Feeling;
 
+        /// <summary>
+        /// Enum with possible feelings that can be chosen by the user to describe their mood.
+        /// </summary>
         enum PossibleFeeling
         {
             Happy,
