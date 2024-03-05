989263a964bf765f7847d272b622a687af5e2e43
Keyance <klara.konopacova@seznam.cz>
2024-03-05 11:49:36 +0100

    Connection to text + database designed

---
+++ HelpLibrary/GlobalConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void InitializeConnections(bool databese, bool textFiles)
+        {
+            if (databese)
+            {
+                //TODO - Set up the SQL Connector properly
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+            if (textFiles)
+            {
+                //TODO - Set up the Text connector properly
+                TextConnector text = new TextConnector();
+                Connections.Add(text);
+            }
+
+        }
+
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
